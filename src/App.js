e9571ffae0913db0ccc5222db5bf9c1136a1fd35
shuhei.tachikawa <shuhei.tusx@gmail.com>
2023-09-03 20:50:57 +0900

    s

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { Home } from "./pages/Home";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { Header } from './components/Header'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <header>This is header</header>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/article/:id" element={<Detail />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,59 @@
-import React from "react";
+import React from 'react'
+import classes from '../styles/Home.module.scss'
+
+const posts = [
+  {
+    id: 1,
+    title: '記事タイトル１',
+    thumbnailUrl: '',
+    createdAt: '2023-09-10T09:00:00.000Z',
+    categories: ['React', 'TypeScript'],
+    content: `
+    本文です。本文です。本文です。本文です。本文です。本文です。
+    本文です。本文です。本文です。本文です。本文です。
+
+    本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。
+
+
+    本文です。本文です。本文です。本文です。本文です。本文です。
+    `,
+  },
+]
 
 export const Home = () => {
-  return <div>トップページ</div>;
-};
+  return (
+    <div className="">
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.id} className={classes.list}>
+              <div className={classes.post}>
+                <div className={classes.postImage}>
+                  <img src={post.thumbnailUrl} alt="" />
+                </div>
+                <div className={classes.postContent}>
+                  <div className={classes.postInfo}>
+                    <div className={classes.postDate}>
+                      {new Date(post.createdAt).toLocaleDateString()}
+                    </div>
+                    <div className={classes.postCategories}>
+                      {post.categories.map((category) => {
+                        return (
+                          <div key={category} className={classes.postCategory}>
+                            {category}
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </div>
+                  <div className={classes.postTitle}>{post.title}</div>
+                  <div className={classes.postBody}>{post.content}</div>
+                </div>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
