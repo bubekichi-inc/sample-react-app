30f4ab698f1cb10b1000c114698657766dace224
shuhei.tachikawa <shuhei.tusx@gmail.com>
2023-09-06 20:03:47 +0900

    s

--- src/pages/Contact/Label.jsx
+++ src/pages/Contact/Label.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Label = (props) => {
   return (
     <label htmlFor={props.htmlFor} className="w-[240px]">
-      {props.text}:
+      {props.text}
     </label>
   )
 }
